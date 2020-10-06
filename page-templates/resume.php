aa81d04ec41b995d5b79b941b9eb565faca0e11f
Andrew R Emery <andrew@arevisual.com>
2020-10-06 12:52:51 -0400

    :sparkles: Set up resume horizontal scroll using static html

---
+++ page-templates/resume.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Template Name: Resume
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package ARE_Visual
+ */
+
+get_header();
+?>
+
+<main id="primary" class="site-main">
+	<h1><?php the_title(); ?></h1>
+	
+  <section id="resume-outer-container">
+		<section id="resume-inner-container">
+			<article class="resume-item one">
+
+			</article>
+			<article class="resume-item two">
+
+			</article>
+
+			<article class="resume-item one">
+
+			</article>
+			<article class="resume-item two">
+
+			</article>
+
+			<article class="resume-item one">
+
+			</article>
+			<article class="resume-item two">
+
+			</article>
+		</section>
+
+  </section>
+	</main>
+	
+
+<?php
+get_sidebar();
+get_footer();
